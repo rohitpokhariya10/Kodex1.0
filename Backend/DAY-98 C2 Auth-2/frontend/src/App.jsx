174a90cf610b77e995630e14a63000e3317aec4b
Rohit Singh Pokhariya <rohit.pokhariya123@gmail.com>
2026-05-23 22:33:51 +0530

    fix BD  bugs

--- Backend/DAY-98 C2 Auth-2/frontend/src/App.jsx
+++ Backend/DAY-98 C2 Auth-2/frontend/src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
   };
 
   const signInHandler = () => {
-    window.location.href = "https://kodex1-0.onrender.com/api/auth/google";
+    window.location.href = "https://kodex1-0.onrender.com/api/auth/google/callback";
   };
 
   return (
