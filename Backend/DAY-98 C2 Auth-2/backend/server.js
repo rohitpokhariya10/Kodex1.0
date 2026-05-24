203b645ce76001cb538c29cf93896728e0d92bf4
asynchronousRohit <rohit.pokhariya123@gmail.com>
2026-05-24 13:18:30 +0530

    Delete Backend/DAY-98 C2 Auth-2 directory

--- Backend/DAY-98 C2 Auth-2/backend/server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./src/app")
-const connectToDb = require("./src/config/database")
-
-app.listen(3000 , ()=>{
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
-
-
-connectToDb()
